cad7d056c822cab4f8438b55aee7e90f2e1cf032
Stephen Florian <steezeven@gmail.com>
2018-08-02 13:57:43 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>OMFG I FINALLY GOT REACT NATIVE SETUP</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Hoooray</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
