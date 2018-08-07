cff33ff8f8e1fbd7b6697565839df3cf7cc83a65
Stephen Florian <steezeven@gmail.com>
2018-08-07 16:31:50 -0600

    moved state to a better location but now need to move the logic into the constructor, as componentWillMount is being deprecated

--- games/rotating-letters/RotatingLetters.js
+++ games/rotating-letters/RotatingLetters.js
@@ -51,6 +51,39 @@ const styles = StyleSheet.create({
   }
 })
 
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const rotations = ['90deg', '180deg', '270deg']
+const colors = ['#90caf9', '#80cbc4', '#b39ddb', '#ffcdd2', '#c8e6c9', '#ffccbc', '#fff9c4']
+
+// const generateUniqueLetterArray = () => {
+//   const uniques = []
+//   while (uniques.length < 3){
+//     const randomNum = Math.floor(Math.random() * 25)
+//     if (!uniques.includes(randomNum) && randomNum !== this.state.activeLetter){
+//       uniques.push(randomNum)
+//     }
+//   }
+//   return shuffle([this.state.activeLetter, ...uniques])
+// }
+
+// const generateUniqueBackgroundColorArray = () => {
+//   const colors = []
+//   while (colors.length < 5) {
+//     const randomNum = Math.floor(Math.random() * 7)
+//     if (!colors.includes(randomNum)){
+//       colors.push(randomNum)
+//     }
+//   }
+//   return colors
+// }
+
+// const generateRandomActiveLetter = () => {
+//   return Math.floor(Math.random() * 25)
+// }
+
+// const generateRandomRotation = () => {
+//   return Math.floor(Math.random() * 3)
+// }
 
 export default class RotatingLetters extends React.Component {
 
@@ -62,27 +95,44 @@ export default class RotatingLetters extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      activeLetter: Math.floor(Math.random() * 25),
-      rotation: Math.floor(Math.random() * 3),
+      activeLetter: 0,
+      letterList: [],
+      colorList: [],
+      rotation: 0,
       score: 0,
       isModalVisible: false,
       correctAnswer: false,
     }
   }
 
+  componentWillMount(){
+    const activeLetter = this.generateRandomActiveLetter()
+    this.setState({
+      activeLetter,
+      letterList: this.generateUniqueLetterArray(activeLetter),
+      colorList: this.generateUniqueBackgroundColorArray(),
+      rotation: this.generateRandomRotation()
+    })
+  }
+
   setModalVisible(visible){
     this.setState({isModalVisible: visible})
   }
 
-  generateUniqueLetterArray = () => {
+  generateUniqueLetterArray = (activeLetter) => {
     const uniques = []
     while (uniques.length < 3){
       const randomNum = Math.floor(Math.random() * 25)
-      if (!uniques.includes(randomNum) && randomNum !== this.state.activeLetter){
+      if (!uniques.includes(randomNum) && randomNum !== activeLetter){
         uniques.push(randomNum)
       }
     }
-    return shuffle([this.state.activeLetter, ...uniques])
+
+    console.log('yo')
+
+    console.log(activeLetter, uniques, 'here')
+
+    return shuffle([activeLetter, ...uniques])
   }
 
   generateUniqueBackgroundColorArray = () => {
@@ -96,14 +146,17 @@ export default class RotatingLetters extends React.Component {
     return colors
   }
 
+  generateRandomActiveLetter = () => {
+    return Math.floor(Math.random() * 25)
+  }
+
+  generateRandomRotation = () => {
+    return Math.floor(Math.random() * 3)
+  }
+
   render() {
 
-    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    const rotations = ['90deg', '180deg', '270deg']
-    const colors = ['#90caf9', '#80cbc4', '#b39ddb', '#ffcdd2', '#c8e6c9', '#ffccbc', '#fff9c4']
-    const colorIndexes = this.generateUniqueBackgroundColorArray()
-
-    console.log(colorIndexes)
+    console.log(this.state, 'render')
 
     return (
     <View style={styles.rotatingLettersContainer}>
@@ -128,9 +181,9 @@ export default class RotatingLetters extends React.Component {
       }]}>
         <Text style={styles.rotateLetterMain}>{letters[this.state.activeLetter]}</Text>
       </View>
-      <View style={styles.letterRow}>{this.generateUniqueLetterArray().map((num, i) => {
-        const background = colors[Math.floor(Math.random() * 6)]
-        console.log(background)
+      <View style={styles.letterRow}>{this.state.letterList.map((num, i) => {
+
+
         return (
           <TouchableOpacity onPress={() => {
             if (num === this.state.activeLetter){
@@ -140,7 +193,7 @@ export default class RotatingLetters extends React.Component {
               console.log(`You pickes ${letters[num]} but the correct answer was ${letters[this.state.activeLetter]}`)
             }
           }} key={i}>
-            <View  style={[styles.letterBtn, {backgroundColor: colors[colorIndexes[i]]}]}>
+            <View  style={[styles.letterBtn, {backgroundColor: colors[this.state.colorList[i]]}]}>
               <Text style={styles.gameLetter}>{letters[num]}</Text>
             </View>
           </TouchableOpacity>
