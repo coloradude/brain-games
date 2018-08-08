720dddedd9b339036f0e15fb8b8c3c39826910c5
Stephen Florian <steezeven@gmail.com>
2018-08-08 00:32:23 -0600

    improved user flow for end of game sequence

--- games/rotating-letters/RotatingLetters.js
+++ games/rotating-letters/RotatingLetters.js
@@ -21,6 +21,7 @@ import shuffle from 'lodash.shuffle'
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const rotations = ['90deg', '180deg', '270deg']
 const colors = ['#90caf9', '#80cbc4', '#b39ddb', '#ffcdd2', '#c8e6c9', '#ffccbc', '#fff9c4']
+const numberOfMovesPerGame = 3
 
 
 const SvgGenerator = params => (
@@ -85,6 +86,7 @@ export default class RotatingLetters extends React.Component {
       timesPlayed: 0,
       isModalVisible: false,
       correctAnswer: false,
+      endGame: false,
     }
   }
 
@@ -94,34 +96,34 @@ export default class RotatingLetters extends React.Component {
 
   handleAnswer = answer => {
 
-    if (this.state.timesPlayed === 9 && answer){
-      console.log('a')
+    if (answer){
       this.setState({
         score: this.state.score + 1,
         timesPlayed: this.state.timesPlayed + 1,
-        isModalVisible: false
+        isModalVisible: true
       })
-    } else if (this.state.timesPlayed === 9 && !answer){
-      console.log('b')
+    } else {
       this.setState({
-        timesPlayed: this.state.timesPlayed + 1
-      })
-    } else if (answer && this.state.timesPlayed < 9){
-      console.log('c')
-      this.setState({
-        isModalVisible: true,
-        correctAnswer: true,
-        score: this.state.score + 1,
-        timesPlayed: this.state.timesPlayed + 1
-      })
-    } else if (!answer && this.state.timesPlayed < 9){
-      console.log('d')
-      this.setState({
-        isModalVisible: true,
-        correctAnswer: false,
-        timesPlayed: this.state.timesPlayed + 1
+        timesPlayed: this.state.timesPlayed + 1,
+        isModalVisible: true
       })
     }
+    // } else if (answer && this.state.timesPlayed < numberOfMovesPerGame){
+    //   console.log('c')
+    //   this.setState({
+    //     isModalVisible: true,
+    //     correctAnswer: true,
+    //     score: this.state.score + 1,
+    //     timesPlayed: this.state.timesPlayed + 1
+    //   })
+    // } else if (!answer && this.state.timesPlayed < numberOfMovesPerGame){
+    //   console.log('d')
+    //   this.setState({
+    //     isModalVisible: true,
+    //     correctAnswer: false,
+    //     timesPlayed: this.state.timesPlayed + 1
+    //   })
+    // }
   }
 
   refreshGameBoard = () => {
@@ -145,7 +147,15 @@ export default class RotatingLetters extends React.Component {
       isModalVisible: false,
       score: 0,
       timesPlayed: 0,
-      correctAnswer: false
+      correctAnswer: false,
+      endGame: false,
+    })
+  }
+
+  endGame = () => {
+    this.setState({
+      endGame: true,
+      isModalVisible: false,
     })
   }
 
@@ -153,9 +163,9 @@ export default class RotatingLetters extends React.Component {
     return (
       <View style={styles.rotatingLettersContainer}>
 
-        <Text>{`Score: ${this.state.score}/10`}</Text>
+        <Text>{`Score: ${this.state.score}/${numberOfMovesPerGame}`}</Text>
 
-        {this.state.timesPlayed === 10 && 
+        {this.state.endGame && 
         <Modal
           animationIn='slideInUp'
           animationOut='slideOutUp'
@@ -163,7 +173,7 @@ export default class RotatingLetters extends React.Component {
           style={styles.modal}
         >
           <View style={styles.modalBackground}>
-            <Text>{`You got ${this.state.score}/10 answers correct`}</Text>
+            <Text>{`You got ${this.state.score}/${numberOfMovesPerGame} answers correct`}</Text>
             <TouchableOpacity onPress={this.restartGame}><Text>Play Again</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => this.props.navigation.navigate('GameGrid')}><Text>Back To Game Menu</Text></TouchableOpacity>
           </View>
@@ -180,7 +190,7 @@ export default class RotatingLetters extends React.Component {
           <View style={styles.modalBackground}>
             {this.state.correctAnswer ? SvgGenerator(checkCircle) : SvgGenerator(timesCircle)}
             <Text style={styles.modalText}>{this.state.correctAnswer ? 'Good job, you selected the right answer!' : 'Better luck next time bucko'}</Text>
-            <TouchableOpacity style={styles.nextButton} onPress={this.refreshGameBoard}>
+            <TouchableOpacity style={styles.nextButton} onPress={this.state.timesPlayed < numberOfMovesPerGame ? this.refreshGameBoard : this.endGame}>
               <Text style={styles.nextButtonText}>Next</Text>
             </TouchableOpacity>
           </View>
